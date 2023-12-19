cf42f84f43881a08cbdd34d57b9ef4c0bdaf1d77
Prajwal <prajwalpawar2303@gmail.com>
2023-12-19 18:35:38 +0530

    added debouncing to search

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,28 +8,36 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    // omdb api url
-    const url = `http://www.omdbapi.com/?s=${search}&apikey=b93b756`;
+    /* debouncing is when we press any button for search, instead of sending it right away stacking search request on each other, it waits for 300 milsec 
+    before sending search request */
+    // using setTimeout/ debouncing search for optimizing search results
+    const debounce = setTimeout(() => {
+      // omdb api url
+      const url = `http://www.omdbapi.com/?s=${search}&apikey=b93b756`;
 
-    // fetching movies from api
-    const fetchMovies = async () => {
-      try {
-        setLoading(true);
+      // fetching movies from api
+      const fetchMovies = async () => {
+        try {
+          setLoading(true);
 
-        const response = await fetch(url);
-        const data = await response.json();
+          const response = await fetch(url);
+          const data = await response.json();
 
-        setMovies(data.Search);
-        setLoading(false);
-      } catch (err) {
-        console.error("Error fetching movies:", error);
-        setLoading(false);
-      }
-    };
+          setMovies(data.Search);
+          setLoading(false);
+        } catch (err) {
+          console.error("Error fetching movies:", error);
+          setLoading(false);
+        }
+      };
 
-    console.log(movies);
+      console.log(movies);
 
-    fetchMovies();
+      fetchMovies();
+    }, 300);
+
+    // Clear the timer on component unmount or when search changes
+    return () => clearTimeout(debounce);
   }, [search]);
 
   return (
